cad85178922e16a0faf02af2a74536c6b8921226
f345104v <jonathanvides88@gmail.com>
2023-09-21 22:09:08 -0500

    added prompts for rounds

--- javascript.js
+++ javascript.js
@@ -47,14 +47,14 @@ function playRound(playerSelection, computerSelection){
 }
 
 //request selection from player and turns it into lower case for comparison 
-const playerSelection = 'rock'//window.prompt("You have been challenged to a best out of 5 by the machine! \n Choose Rock, Paper or Scissors").toLowerCase();
+//const playerSelection = 'rock'//window.prompt("You have been challenged to a best out of 5 by the machine! \n Choose Rock, Paper or Scissors").toLowerCase();
 const computerSelection = [getComputerChoice(),getComputerChoice(),getComputerChoice(),getComputerChoice(),getComputerChoice()];
 
-console.log(playRound(playerSelection, computerSelection[1]));
-console.log(playRound(playerSelection, computerSelection[2]));
-console.log(playRound(playerSelection, computerSelection[3]));
-console.log(playRound(playerSelection, computerSelection[4]));
-console.log(playRound(playerSelection, computerSelection[0]));
+//console.log(playRound(playerSelection, computerSelection[1]).charAt(4));
+//console.log(playRound(playerSelection, computerSelection[2]).charAt(4));
+//console.log(playRound(playerSelection, computerSelection[3]).charAt(4));
+//console.log(playRound(playerSelection, computerSelection[4]).charAt(4));
+//console.log(playRound(playerSelection, computerSelection[0]).charAt(4));
 
 // make 2 variables that will keep the score of player and computer
 
@@ -63,5 +63,22 @@ let computerScore = 0 ;
 let playerScore = 0 ;
 
 //get 5th letter of string return from play round function if w add 1 to player score if l add 1 to computer score
+function game(){
+    alert("You have been challenged to a best out of 5 by the machine! \n Choose Rock, Paper or Scissors")
+    
+    playerSelection = window.prompt("Round 1! Rock, Paper or Scissors!").toLowerCase();
+if (playRound(playerSelection, computerSelection[1]).charAt(4)=="W"){
+       return playerScore + 1;
+} else if (playRound(playerSelection, computerSelection[1]).charAt(4)=="L"){
+        return computerScore + 1;
+}
+playerSelection = window.prompt("Round 2! Rock, Paper or Scissors!").toLowerCase();
+//playRound(playerSelection, computerSelection[2]).charAt(4));
+//playRound(playerSelection, computerSelection[3]).charAt(4));
+//playRound(playerSelection, computerSelection[4]).charAt(4));
+//playRound(playerSelection, computerSelection[0]).charAt(4));
+}
 
+game()
 
+console.log("CPU "+computerScore,"YOU "+playerScore);
